8022151a9114dbd21bc01bf2eaff07b525fda6fb
Vadim Tiganov <vadimyxy@gmail.com>
2024-12-25 22:46:51 +0300

    bubble sort + test

---
+++ tests/bubble_sort_test.py
@@ -0,0 +1,31 @@
+import unittest
+from codefiles.bubble_sort import bubbleSort
+
+class TestBubbleSort(unittest.TestCase):
+
+    def test_empty_list(self):
+        self.assertEqual(bubbleSort([]), [])
+
+    def test_single_element(self):
+        self.assertEqual(bubbleSort([1]), [1])
+
+    def test_sorted_list(self):
+        self.assertEqual(bubbleSort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+
+    def test_reverse_sorted_list(self):
+        self.assertEqual(bubbleSort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
+
+    def test_unsorted_list(self):
+        self.assertEqual(bubbleSort([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]), [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9])
+
+    def test_list_with_duplicates(self):
+        self.assertEqual(bubbleSort([4, 4, 4, 4]), [4, 4, 4, 4])
+
+    def test_negative_numbers(self):
+        self.assertEqual(bubbleSort([-3, -1, -4, -1, -5, -9, -2, -6, -5, -3, -5]), [-9, -6, -5, -5, -5, -4, -3, -3, -2, -1, -1])
+
+    def test_mixed_numbers(self):
+        self.assertEqual(bubbleSort([3, -1, 4, 1, -5, 9, -2, 6, -5, 3, 5]), [-5, -5, -2, -1, 1, 3, 3, 4, 5, 6, 9])
+
+if __name__ == '__main__':
+    unittest.main()
